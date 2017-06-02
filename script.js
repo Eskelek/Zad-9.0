72f6169d100f3902e2fb0d08a7dff6c7b8e485bb
Eskelek <lorthan966@wp.plgit config user.email lorthan966@wp.plgit status>
2017-06-02 21:06:14 +0200

    star

---
+++ script.js
@@ -0,0 +1,10 @@
+function drawTree(h) {
+    for ( i=0; i<=h ; i++ ) {
+        var star = '';
+        for (j=0 ; j<i ; j++){
+            var star = star +'*';
+        }
+        console.log(star);
+    }
+}
+drawTree(5);
