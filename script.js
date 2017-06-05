945086934ce7c58da6b9dd963060797cc01b16bc
Eskelek <lorthan966@wp.plgit config user.email lorthan966@wp.plgit status>
2017-06-05 16:03:12 +0200

    correction

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
-var list = document.getElementById('list')
-var add = document.getElementById('addElem')
-add.addEventListener('click', function() {
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+add.addEventListener('click', function () {
     var element = document.createElement('li');
     var numb = document.getElementsByTagName('li').length;
     element.innerHTML = "item " + numb;
